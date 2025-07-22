788351b8080bc25e240cce4a1f5e6988a5c4f43e
Wambui Kibathi <zeldabobo02@gmail.com>
2025-07-23 00:14:31 +0300

    WIP: Add state, handleSubmit and handleChange logic

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -13,6 +13,11 @@ export function RegisterForm({ onRegister }) {
             setError('Please enter a valid username');
         }
     };
+
+    const handleChange = (e) => {
+        setUsername(e.target.value);
+        if (error) setError('');
+    };
     
     return (
         <>
